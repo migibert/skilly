53318d689db8e5d12e9477177f16afe278454433
Mikael Gibert <mikael@teemo.co>
2019-06-24 17:59:41 +0200

    Add ClippedDrawer as menu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ClippedDrawer from './menu/ClippedDrawer'
 
 function App() {
   return (
     <div className="App">
-
+        <ClippedDrawer></ClippedDrawer>
     </div>
   );
 }
