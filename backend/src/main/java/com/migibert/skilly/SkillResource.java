cced1e83522d78ba96b1f01fa3c07ac20ad00538
Mikael Gibert <mikael@teemo.co>
2019-07-10 13:50:17 +0200

    Init quarkus backend

---
+++ backend/src/main/java/com/migibert/skilly/SkillResource.java
@@ -0,0 +1,16 @@
+package com.migibert.skilly;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/organizations/{organizationId}/skills")
+public class SkillResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ backend/src/test/java/com/migibert/skilly/NativeSkillResourceIT.java
@@ -0,0 +1,9 @@
+package com.migibert.skilly;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeSkillResourceIT extends SkillResourceTest {
+
+    // Execute the same tests but in native mode.
+}
